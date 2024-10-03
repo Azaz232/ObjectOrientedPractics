ca2428293bc770f1f4fd59bb7e028bf80cb87b6b
Azaz232 <sofasennikova85692@gmail.com>
2024-10-04 02:11:20 +0700

    ..

---
+++ src/WinFormsApp1/View/Controls/AddressControl.cs
@@ -0,0 +1,32 @@
+﻿using ObjectOrientedPractics.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedPractics.View.Controls
+{
+    public partial class AddressControl : UserControl
+    {
+        /// <summary>
+        /// Variable type - Address.
+        /// </summary>
+        private Address _address = new Address();
+
+        /// <summary>
+        /// Gets and sets an address.
+        /// </summary>
+        public Address OurAddress { get { return _address; } set { _address = value; } }
+
+
+        public AddressControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
